8e5870e89fd6716a334f1dbe7dcd638460c540f1
CrisBlogzbi <crisbogdan2001@outlook.com>
2023-09-07 19:42:10 +0200

    Add function for styling selected answer(s)

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function selectAnswer() {
-
-}
+    this.closest('.question').querySelectorAll('.answer').forEach(ans => {
+      ans.classList.remove('selected');
+    });
+    this.classList.add('selected');
+  }
+  
+  document.querySelectorAll('.answer').forEach(answer => {
+    answer.addEventListener('click', selectAnswer);
+  });
 
 function revealAnswers() {
 
